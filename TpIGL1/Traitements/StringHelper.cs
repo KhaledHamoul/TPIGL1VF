fbdd86806b692e6d91dbbf8a3897fd547559653e
SalaheddineDz <fs_boukhoulda@esi.dz>
2017-10-22 01:51:37 +0100

    test conflit 4 salahBr

--- TpIGL1/Traitements/StringHelper.cs
+++ TpIGL1/Traitements/StringHelper.cs
@@ -257,6 +257,7 @@ namespace TpIGL1.Traitements
                 }
                 if (motArg.Length % 2 != 0) nouveauMot += motArg[motArg.Length - 1];
             }
+            /// commentaire different de celui qui existe dans khaledBr
             catch (Exception exp)
             {
                 throw new Exception(exp.Message);
