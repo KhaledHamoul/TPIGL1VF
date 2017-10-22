b8d339ac1e6d00180cad345bc86ecfbcaf472fb4
SalaheddineDz <fs_boukhoulda@esi.dz>
2017-10-22 03:00:29 +0100

    .

--- TpIGL1/Traitements/StringHelper.cs
+++ TpIGL1/Traitements/StringHelper.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using TpIGL1.Data;
 
 namespace TpIGL1.Traitements
-{ 
+{/ 
     /// <summary>
     /// Cette classe contient diverses methodes qui traitent les chaines de characteres
     /// </summary>
