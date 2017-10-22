ea23f18743ba6bd086a40420c65a9ad0681d196d
khaledHamoul <fk_hamoul@esi.dz>
2017-10-22 01:38:37 +0100

    test conflit 2 khaledBr

--- TpIGL1/Traitements/StringHelper.cs
+++ TpIGL1/Traitements/StringHelper.cs
@@ -257,9 +257,9 @@ namespace TpIGL1.Traitements
                 }
                 if (motArg.Length % 2 != 0) nouveauMot += motArg[motArg.Length - 1];
             }
-            catch (Exception exp)
+            catch (Exception expeption)
             {
-                throw new Exception(exp.Message);
+                throw new Exception(expeption.Message);
             }
             motArg = nouveauMot;
         }
